6758d4bed865c3297af118fef6833249ac746828
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-05-28 23:14:25 +0200

    add doses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/cocktails', to: 'cocktails#index'
-  get '/cocktails/:id', to: 'cocktails#show'
-  get '/cocktails/new', to: 'cocktails#new'
-  post '/cocktails', to: 'cocktails#create'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
